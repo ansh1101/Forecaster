daff3babc782867410d0682e7241a7f48c14991c
Ansh Singh <81492907+ansh1101@users.noreply.github.com>
2021-07-27 00:18:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+let weather ={
+    "apikey" : "76394aa4a5a443205c3632dda0bba766",
+    fetchWeather:function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city 
+            + "&units=metric&appid="
+            +this.apikey
+
+        )
+        .then((response) => {
+            if (!response.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return response.json();
+          })
+          .then((data) => this.displayWeather(data));
+
+
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const {icon,description } = data.weather[0];
+        const { temp,humidity} = data.main;
+        const{speed} = data.wind;
+         console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name
+        document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+  
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+    
